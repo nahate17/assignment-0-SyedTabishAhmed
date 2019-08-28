80e32bf85d5e8783dd6ad0a59c7b2ee78a54a422
Syed Tabish Ahmed <z1886780@tiger.cs.niu.edu>
2019-08-27 19:06:17 -0500

    First commit

--- assignment00.cc
+++ assignment00.cc
@@ -1,8 +1,18 @@
+/* Syed TAbish Ahmed
+ * Z!886780
+ * CSCI 340 Data Struct Algorithm Analysis
+ * Class # 8213-Section PE1-Component LEC
+ *
+ * I certify that this is my own work and where appropiate an extension of the starter code provided for this assignment.
+ */
+
 #include <iostream>
 using namespace std;
 int main()
 {
 	cut << "Hello, world" << endl;
+	cut <<"Data Structure are Important!!"<< endl;
+	cout <<"Data Structure are Imporatan!!"<< endl:
 	return 0;
 }
 
